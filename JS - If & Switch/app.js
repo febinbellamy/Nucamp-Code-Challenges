905a465d760861541898ec4e8af740ba177f90ff
Febin Bellamy <febinbellamy@gmail.com>
2021-12-30 08:05:05 -0500

    code challenges

---
+++ JS - If & Switch/app.js
@@ -0,0 +1,30 @@
+/* --------------- Challenge 1 --------------*/
+
+const age = 15;
+
+if (age >= 16) {
+    console.log("You are old enough to drive.");
+} else {
+    console.log("You are not yet old enough to drive.");
+}
+/* ----------- End of Challenge 1 -----------*/
+
+
+/* --------------- Challenge 2 --------------*/
+const drinkSize = "small";
+
+switch(drinkSize) {
+    case "small":
+        console.log("A small drink is 10 ounces.");
+        break;
+    case "medium": 
+        console.log("A medium drink is 16 ounces.");
+        break;
+    case "large":
+        console.log("A large drink is 22 ounces.");
+        break;
+    default:
+        console.log("Unknown drink size.");
+}
+/* ----------- End of Challenge 2 -----------*/
+
