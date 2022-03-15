9950145a15df9d5ff57dc4cf72a78ea3e9427b93
Febin Bellamy <febinbellamy@gmail.com>
2022-03-15 12:07:02 -0400

    useEffect

--- React Hooks/react-hooks-demo/src/App.js
+++ React Hooks/react-hooks-demo/src/App.js
@@ -1,11 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+function ExampleChild() {
+  useEffect(() => {
+    console.log("Subscribing..");
+
+    return () => {
+      console.log("Unsubscribing..");
+    };
+  }, []);
+
+  return <h2>Example child</h2>;
+}
 
 function App() {
   const [color, setColor] = useState("red");
-  const [msg, setMsg] = useState("");
+  const [msg, setMsg] = useState("Initial Message");
 
-  function handleClick() {
+  function changeColor() {
     color === "red" ? setColor("blue") : setColor("red");
+  }
+
+  function changeMsg() {
     setMsg("Secret Message");
   }
 
@@ -13,7 +28,9 @@ function App() {
     <div>
       <h1 style={{ background: color }}>React Hooks Demo</h1>
       <h1>{msg}</h1>
-      <button onClick={handleClick}>Click Me</button>
+      <button onClick={changeColor}>Click Me</button>
+      <button onClick={changeMsg}>Click Me</button>
+      {msg === "Initial Message" && <ExampleChild />}
     </div>
   );
 }
